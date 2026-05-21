f08d57b616d7435ee6d9929771ada9c382b2060f
Brice Yan <98643308+briceyan@users.noreply.github.com>
2026-05-21 10:30:50 +0800

    build: sync zed extension to 0.0.13 (#6)

--- languages/toolang/indents.scm
+++ languages/toolang/indents.scm
@@ -1,5 +1,7 @@
 ; Source of truth for Toolang Tree-sitter indentation queries.
 ; Synced copy for the Zed extension. Edit the grammar repository instead.
 
-(declaration_header) @indent
-(thunk_signature) @indent
+(struct) @indent
+(instruct) @indent
+(thunk) @indent
+(block) @indent
